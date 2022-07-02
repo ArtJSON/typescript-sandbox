5404e5cb987cf71e10e5519511d28e416cce82af
ArtJSON <artur.pietrzak1@outlook.com>
2022-07-02 15:06:18 +0200

    Add functions, objects etc

--- first-project/src/index.ts
+++ first-project/src/index.ts
@@ -21,3 +21,50 @@ let prodid: string | number | boolean;
 prodid = "22";
 prodid = 22;
 prodid = true;
+
+// enums
+enum en1 {
+  Up, // 0
+  Down, // 1
+  Left,
+  Right,
+}
+
+enum en2 {
+  Up = 1,
+  Down, // 2
+  Left, // 3
+  Right, // 4
+}
+
+enum en3 {
+  Up = "Up",
+  Down = "Down",
+  Left = "Left",
+  Right = "Right",
+}
+
+// Object
+type User = {
+  id: number;
+  name: string;
+};
+
+const user: User = {
+  id: 3,
+  name: "John",
+};
+
+// Type assertion
+let cid: any = 1;
+let customerid1 = <number>cid;
+let customerid2 = cid as number;
+
+// Functions
+function addNum(x: number, y: number): number {
+  return x + y;
+}
+
+function log(message: string | number): void {
+  console.log(message);
+}
